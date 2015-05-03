7e273f01a8cc1979978ba5f2b62df6c12bbec907
slaterade <w.richard.slater@gmail.com>
2015-05-03 17:27:37 -0500

    primes

--- hello.rs
+++ hello.rs
@@ -1,3 +1,14 @@
 fn main() {
-	println!("Hello world!");
+	for i in 2..100 {
+		let mut is_prime = true;
+		for j in 2..(i/2)+1 {
+			if i % j == 0 {	
+				is_prime = false;
+				break;
+			}
+		}
+		if is_prime {
+			println!("{} is prime.", i);
+		}
+	}
 }
